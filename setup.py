dfc027c441a03a1927861e9ce4f396568770bf6a
Gregory Hellings <greg.hellings@gmail.com>
2019-02-18 17:59:47 -0600

    Update the maintenance
    Auto-deploy with Travis
    Update version line for next release
    Ignore build artifact folders
    Elminiate duplication of README into setup.py

--- setup.py
+++ setup.py
@@ -22,6 +22,10 @@ with open(os.path.join(here, 'dnszone', 'dnszone.py')) as f:
     for line in f:
         if line.startswith('__version__ = '):
             __version__ = line.split(' = ')[-1].strip(" '\"\n")
+with open(os.path.join(here, 'README.md')) as f:
+    readme_lines = []
+    for line in f:
+        readme_lines.append(f)
 
 
 class TestCommand(Command):
@@ -80,121 +84,7 @@ setup_args = dict(
     author='Greg Hellings',
     author_email='greg.hellings@gmail.com',
     description='Easy Zone - DNS Zone abstraction module',
-    long_description='''\
-dnszone
-========
-
-Overview
---------
-
-DNSZone is a package to manage the common record types of a
-zone file, including SOA records.  This module sits on top of
-the dnspython package and provides a higher level abstraction
-for common zone file manipulation use cases.
-
-http://pypi.python.org/pypi/dnszone
-https://github.come/greg-hellings/dnszone
-
-
-Main features:
-
-* A high-level abstraction on top of dnspython.
-* Load a zone file into objects.
-* Modify/add/delete zone/record objects.
-* Save back to zone file.
-* Auto-update serial (if necessary).
-
-
-Requirements
-------------
-
-  * dnspython - http://www.dnspython.org/
-
-
-Build/Test/Install
-------------------
-
-Build::
-
-  $ python setup.py build
-
-Test::
-
-  $ python setup.py test
-
-Install::
-
-  $ python setup.py install
-
-
-OR with setuptools::
-
-  $ easy_install dnszone
-
-
-Examples
---------
-
-dnszone::
-
-  >>> from dnszone import dnszone
-  >>> z = dnszone.zone_from_file('example.com', '/var/namedb/example.com')
-  >>> z.domain
-  'example.com.'
-  >>> z.root.soa.serial
-  2007012902L
-  >>> z.root.records('NS').items
-  ['ns1.example.com.', 'ns2.example.com.']
-  >>> z.root.records('MX').items
-  [(10, 'mail.example.com.'), (20, 'mail2.example.com.')]
-  >>> z.names['foo.example.com.'].records('A').items
-  ['10.0.0.1']
-
-  >>> ns = z.root.records('NS')
-  >>> ns.add('ns3.example.com.')
-  >>> ns.items
-  ['ns1.example.com.', 'ns2.example.com.', 'ns3.example.com.']
-  >>> ns.delete('ns2.example.com')
-  >>> ns.items
-  ['ns1.example.com.', 'ns3.example.com.']
-
-  >>> z.save(autoserial=True)
-
-ZoneCheck::
-
-  >>> from dnszone.zone_check import ZoneCheck
-  >>> c = ZoneCheck()
-  >>> c.isValid('example.com', '/var/named/zones/example.com')
-  True
-  >>> c.isValid('foo.com', '/var/named/zones/example.com')
-  False
-  >>> c.error
-  'Bad syntax'
-  >>>
-  >>> c = ZoneCheck(checkzone='/usr/sbin/named-checkzone')
-  >>> c.isValid('example.com', '/var/named/zones/example.com')
-  True
-  >>>
-
-ZoneReload::
-
-  >>> from dnszone.zone_reload import ZoneReload
-  >>> r = ZoneReload()
-  >>> r.reload('example.com')
-  zone reload up-to-date
-  >>> r.reload('foo.com')
-  rndc: 'reload' failed: not found
-  Traceback (most recent call last):
-    File "<stdin>", line 1, in <module>
-    File "dnszone/zone_reload.py", line 51, in reload
-      raise ZoneReloadError("rndc failed with return code %d" % r)
-  dnszone.zone_reload.ZoneReloadError: rndc failed with return code 1
-  >>>
-  >>> r = ZoneReload(rndc='/usr/sbin/rndc')
-  >>> r.reload('example.com')
-  zone reload up-to-date
-  >>>
-''',
+    long_description=readme_lines.join('\n'),
     url='https://github.com/greg-hellings/dnszone',
     packages=['dnszone'],
     cmdclass={'test': TestCommand, 'clean': CleanCommand},
